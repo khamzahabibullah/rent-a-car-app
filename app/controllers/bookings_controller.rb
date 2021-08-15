853fd66e359fee4ef02898dbd277149acb6ee080
Khamza Habibullah <habibullahkhamza@gmail.com>
2021-08-15 15:22:06 +0400

    bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  def create
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+
+    if @booking.save
+      redirect_to @car
+    else
+      render 'cars/show'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :ending_date, :total_price)
+  end
 end
