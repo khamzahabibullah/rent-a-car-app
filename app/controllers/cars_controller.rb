cf6f255031ccbedf5840380680a389b8e6775bac
Octavian Serban <octa_serban@yahoo.com>
2021-08-17 15:01:05 +0400

    implement pundit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,20 +1,25 @@
 class CarsController < ApplicationController
+skip_before_action :authenticate_user!, only: %i[ index show ]
 
   def index
-    @cars = Car.all
+    @cars = policy_scope(Car)
   end
 
   def show
     @car = Car.find(params[:id])
+    authorize @car
   end
 
   def new
     @car = Car.new
+    authorize @car
   end
 
   def create
     @car = Car.new(car_params)
     @car.user = current_user
+
+    authorize @car
     if @car.save
       redirect_to @car
     else
@@ -24,10 +29,12 @@ class CarsController < ApplicationController
 
   def edit
     @car = Car.find(params[:id])
+    authorize @car
   end
 
   def update
     @car = Car.find(params[:id])
+    authorize @car
     if @car.update(car_params)
       redirect_to @car
     else
@@ -37,6 +44,7 @@ class CarsController < ApplicationController
 
   def destroy
     @car = Car.find(params[:id])
+    authorize @car
     @car.destroy
 
     redirect_to @car
